71b9bcdbda5ef1d042df8576ea344a439873fb9d
Jindřich Vodák <jindrich.vodak@email.cz>
2022-03-06 22:11:37 +0100

    redesign

---
+++ DB/RideEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="RideID"></param>
+    /// <param name="StartLocation"></param>
+    /// <param name="EndLocation"></param>
+    /// <param name="TimeOfDeparture"></param>
+    /// <param name="TimeOfArrival"></param>
+    public record RideEntity(Guid RideID,
+        string StartLocation,
+        string EndLocation,
+        DateTime TimeOfDeparture,
+        DateTime TimeOfArrival)
+    {
+        /// <summary>
+        /// Represents the car
+        /// </summary>
+        public CarEntity? Car;
+        /// <summary>
+        /// Represents the driver
+        /// </summary>
+        public UserEntity? Driver;
+        /// <summary>
+        /// Collection of passengers
+        /// </summary>
+        public ICollection<UserEntity>? Passengers { get; init; } = new List<UserEntity>();
+    }
+}
---
+++ DB/UserEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="UserID"></param>
+    /// <param name="Name"></param>
+    /// <param name="Surname"></param>
+    /// <param name="Photo"></param>
+    public record UserEntity(Guid UserID,
+        string Name,
+        string Surname,
+        string? Photo)
+    {
+        /// <summary>
+        /// Collection of owned cars
+        /// </summary>
+        public ICollection<CarEntity>? OwnedCars { get; init; } = new List<CarEntity>();
+        /// <summary>
+        /// Collection of rides driven by this user
+        /// </summary>
+        public ICollection<RideEntity>? DriverRides { get; init; } = new List<RideEntity>();
+        /// <summary>
+        /// Collection of rides that were not driven by this user
+        /// </summary>
+        public ICollection<RideEntity>? PassengerRides { get; init; } = new List<RideEntity>();
+
+    }
+}
