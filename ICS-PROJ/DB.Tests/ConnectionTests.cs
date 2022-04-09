823c1947f64c048bf167cd3c4745568a74971853
swanenko <xlebed11@vutbr.cz>
2022-04-09 21:54:17 +0200

    added first migration

---
+++ ICS-PROJ/DB.Tests/ConnectionTests.cs
@@ -0,0 +1,33 @@
+﻿
+using DB.Contexts;
+using DB.Tests.Seeds;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Xunit;
+
+namespace DB.Tests
+{
+    public class ConnectionTests : BaseTest<IProjectDbContext>
+    {
+        public ConnectionTests()
+        {
+            var dBContextOptions = new DbContextOptionsBuilder<ProjectDbContext>()
+                .UseInMemoryDatabase(nameof(ProjectDbContextTests));
+
+            var context = new TestDbContext(dBContextOptions.Options);
+
+            context.RideEntities.Add(RideSeeds.Ride1);
+            context.CarEntities.Add(CarSeeds.MiniCooper);
+            context.UserEntities.Add(UserSeeds.User1);
+            context.SaveChanges();
+
+            SUT = context;
+        }
+
+        [Fact]
+        public void Connection()
+        {
+            Assert.True(SUT.RideEntities.ToArray().Any());
+        }
+    }
+}
---
+++ ICS-PROJ/DB/Factories/SqlServerDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DB.Contexts;
+
+namespace DB.Factories
+{
+    public class SqlServerDbContextFactory : IDbContextFactory<ProjectDbContext>
+    {
+        private readonly string _connectionString;
+
+        public SqlServerDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ProjectDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProjectDbContext>();
+            optionsBuilder.UseSqlServer(_connectionString);
+
+            //optionsBuilder.LogTo(System.Console.WriteLine); //Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+            //optionsBuilder.EnableSensitiveDataLogging();
+
+            return new ProjectDbContext(optionsBuilder.Options);
+        }
+    }
+}
