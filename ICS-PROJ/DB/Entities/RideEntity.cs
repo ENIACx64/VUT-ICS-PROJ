66e86cd765d8a49425363d942bce613de18b4ce4
Jindřich Vodák <jindrich.vodak@email.cz>
2022-04-09 14:55:38 +0200

    hierarchy update

--- ICS-PROJ/DB/Entities/RideEntity.cs
+++ ICS-PROJ/DB/Entities/RideEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DB
+namespace DB.Entities
 {
     /// <summary>
     /// Constructor
--- ICS-PROJ/DB/Models/Car/CarListModel.cs
+++ ICS-PROJ/DB/Models/Car/CarListModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DB.Enums;
 
 namespace DB.Models.Car
 {
