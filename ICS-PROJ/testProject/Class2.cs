14ccdde7b6458b196f19d784228b1e81fdfff0cd
Jindřich Vodák <jindrich.vodak@email.cz>
2022-04-09 16:06:53 +0200

    blablabla

---
+++ ICS-PROJ/testProject/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testProject
+{
+    internal class Class2
+    {
+    }
+}
