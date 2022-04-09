93b42cb5faf6cb1c9ed0ff6d425dc0b020d36834
Jindřich Vodák <jindrich.vodak@email.cz>
2022-04-09 18:38:13 +0200

    Update RideDetailModel.cs

--- ICS-PROJ/BL/Models/RideDetailModel.cs
+++ ICS-PROJ/BL/Models/RideDetailModel.cs
@@ -8,9 +8,9 @@ namespace BL.Models
 {
     public class RideDetailModel : IModel<Guid>
     {
-        public Guid Id { get; set; }
-        public string StartLocation { get; set; }
-        public string EndLocation { get; set; }
+        public Guid ID { get; set; }
+        public string? StartLocation { get; set; }
+        public string? EndLocation { get; set; }
         public DateTime TimeOfDeparture { get; set; }
         public DateTime TimeOfArrival { get; set; }
 
